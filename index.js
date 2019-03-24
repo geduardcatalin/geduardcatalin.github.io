7f7df072c33c9ba127c5dbf1fd4cdabebe02f5fe
geduardcatalindev <g.eduardcatalin.dev@gmail.com>
2019-03-25 00:37:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const App = () => {
+  return (
+    <div style={{'width':'100%','height':'100%','top':'0','left':'0','z-index':'-999','backgroundColor':'grey'}}><div>
+  );
+}
 
+ReactDOM.render(<App />, document.getElementById('root'));
